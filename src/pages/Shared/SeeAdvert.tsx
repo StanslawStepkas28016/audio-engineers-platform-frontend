ad169b40f8336235a749d1d17afceb9ea6d88650
Stanisław Stepka <stanislawstepka@Macbook-Pro-Stanisaw.local>
2025-07-07 14:55:25 +0200

    Fix styling for multiple pages

--- src/pages/Shared/SeeAdvert.tsx
+++ src/pages/Shared/SeeAdvert.tsx
@@ -5,6 +5,7 @@ import {AlertCircle, Facebook, HandCoins, Instagram, Linkedin} from "lucide-reac
 import {isAxiosError} from "axios";
 import {transformDate, transformPlaylistUrlToEmbedUrl} from "@/hooks/utils.ts";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {LoadingPage} from "@/pages/Shared/LoadingPage.tsx";
 
 export type AdvertData = {
     idUser: string,
@@ -23,18 +24,28 @@ export type AdvertData = {
 
 export const SeeAdvert = ({idUser}: { idUser: string }) => {
     const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
+    const [noAdvertPostedError, setNoAdvertPostedError] = useState("");
     const [advertData, setAdvertData] = useState<AdvertData | null>(null);
 
     const fetchUserAdvert = async () => {
         try {
+            setIsLoading(true);
             const response = await axiosInstance.get(`/advert/by-id-user/${idUser}`);
             setAdvertData(response.data);
         } catch (e) {
             if (isAxiosError(e) && e.response) {
-                setError(e.response.data.ExceptionMessage);
+                if (e.response.status === 500) {
+                    setNoAdvertPostedError(e.response.data.ExceptionMessage);
+                    console.log(e)
+                } else {
+                    setError(e.response.data.ExceptionMessage);
+                }
             } else {
                 console.log(e)
             }
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -42,106 +53,111 @@ export const SeeAdvert = ({idUser}: { idUser: string }) => {
         fetchUserAdvert();
     }, [])
 
+    if (isLoading) {
+        return <LoadingPage/>;
+    }
     return (
-        <div className="flex flex-col items-center justify-center mb-10">
-            <div className="text-center mt-5 -mb-10 px-50">
-                {
-                    advertData && (
-                        <div>
-                            <></>
-
-                            <h1 className="text-3xl font-bold">{advertData.title}</h1>
-
-                            <p className="leading-7 [&:not(:first-child)]:mt-6">
-                                {advertData.userFirstName} {advertData.userLastName} | {transformDate(advertData.dateCreated)}
-                            </p>
-
-                            <p className="leading-7 [&:not(:first-child)]:mt-6 text-justify">{advertData.description}</p>
-
-                            <Card className="mt-10">
-                                <CardHeader>
-                                    <CardTitle className="text-3xl font-bold">Prices starting from:</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="flex flex-row gap-4 items-center justify-center">
-                                        <HandCoins size={100} strokeWidth={1}/>
-                                        <p className="text-2xl ">{advertData.price} <b>PLN</b></p>
-                                    </div>
-                                    <p className="mt-5">
-                                        This is a price for per one <b>{advertData.categoryName}</b> inquiry!
-                                    </p>
-                                </CardContent>
-                            </Card>
-
-                            <iframe
-                                src={transformPlaylistUrlToEmbedUrl(advertData.portfolioUrl)}
-                                className="mt-10 w-full"
-                                height="400"
-                            />
-
-
-                            <h1 className="text-3xl font-bold mt-10 mb-10">Want to collaborate?</h1>
-                            <div className="flex flex-row gap-4 items-center justify-center">
-                                <a href="https://www.instagram.com" target="_blank"><Instagram size={50}
-                                                                                               strokeWidth={2}/></a>
-                                <a href="https://www.instagram.com" target="_blank"><Facebook size={50}
-                                                                                              strokeWidth={2}/></a>
-                                <a href="https://www.instagram.com" target="_blank"><Linkedin size={50}
-                                                                                              strokeWidth={2}/></a>
-                            </div>
-
-                            <h1 className="text-3xl font-bold mt-10">See my reviews!</h1>
-
-                            {/* Reviews */}
-                            <Card className="my-10">
-                                <CardHeader className="">
-                                    <div className="flex justify-between">
-                                        <CardTitle className="text-1xl">William Smith</CardTitle>
-                                    </div>
-                                    <div className="flex justify-between text-sm text-muted-foreground">
-                                        <span>Client</span>
-                                        <span>8 months ago</span>
-                                    </div>
-                                </CardHeader>
-                                <CardContent className="pt-2">
-                                    <p className="text-justify">
-                                        My experience working with this guy was great! I really liked how passionate he was
-                                        about making minor adjustments to a song he mixed for me :)
-                                    </p>
-                                </CardContent>
-                            </Card>
-
-                            <Card className="my-10">
-                                <CardHeader className="">
-                                    <div className="flex justify-between">
-                                        <CardTitle className="text-1xl">Anna Doe</CardTitle>
-                                    </div>
-                                    <div className="flex justify-between text-sm text-muted-foreground">
-                                        <span>Client</span>
-                                        <span>12 months ago</span>
-                                    </div>
-                                </CardHeader>
-                                <CardContent className="pt-2">
-                                    <p className="text-justify">
-                                        My experience working with this guy was great! I really liked how passionate he was
-                                        about making minor adjustments to a song he mixed for me :)
-                                    </p>
-                                </CardContent>
-                            </Card>
-
-                        </div>
-                    )
-                }
-            </div>
-            {error && (
+        <div className="flex flex-col items-center justify-center text-center p-10">
+            {noAdvertPostedError ? (
                 <Alert variant="destructive">
+                    <AlertCircle className="h-4 w-4"/>
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>
+                        {noAdvertPostedError}
+                    </AlertDescription>
+                </Alert>
+            ) : (
+                advertData && (
+                    <div>
+                        <h1 className="text-3xl font-bold">{advertData.title}</h1>
+
+                        <p className="leading-7 [&:not(:first-child)]:mt-6">
+                            {advertData.userFirstName} {advertData.userLastName} | {transformDate(advertData.dateCreated)}
+                        </p>
+
+                        <p className="leading-7 [&:not(:first-child)]:mt-6 text-justify">{advertData.description}</p>
+
+                        <Card className="mt-10">
+                            <CardHeader>
+                                <CardTitle className="text-3xl font-bold">Prices starting from:</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="flex flex-row gap-4 items-center justify-center">
+                                    <HandCoins size={100} strokeWidth={1}/>
+                                    <p className="text-2xl ">{advertData.price} <b>PLN</b></p>
+                                </div>
+                                <p className="mt-5">
+                                    This is a price for per one <b>{advertData.categoryName}</b> inquiry!
+                                </p>
+                            </CardContent>
+                        </Card>
+
+                        <iframe
+                            src={transformPlaylistUrlToEmbedUrl(advertData.portfolioUrl)}
+                            className="mt-10 w-full"
+                            height="400"
+                        />
+
+
+                        <h1 className="text-3xl font-bold mt-10 mb-10">Want to collaborate?</h1>
+                        <div className="flex flex-row gap-4 items-center justify-center">
+                            <a href="https://www.instagram.com" target="_blank"><Instagram size={50}
+                                                                                           strokeWidth={2}/></a>
+                            <a href="https://www.instagram.com" target="_blank"><Facebook size={50}
+                                                                                          strokeWidth={2}/></a>
+                            <a href="https://www.instagram.com" target="_blank"><Linkedin size={50}
+                                                                                          strokeWidth={2}/></a>
+                        </div>
+
+                        <h1 className="text-3xl font-bold mt-10">See my reviews!</h1>
+
+                        {/* Reviews */}
+                        <Card className="my-10">
+                            <CardHeader className="">
+                                <div className="flex justify-between">
+                                    <CardTitle className="text-1xl">William Smith</CardTitle>
+                                </div>
+                                <div className="flex justify-between text-sm text-muted-foreground">
+                                    <span>Client</span>
+                                    <span>8 months ago</span>
+                                </div>
+                            </CardHeader>
+                            <CardContent className="pt-2">
+                                <p className="text-justify">
+                                    My experience working with this guy was great! I really liked how passionate he was
+                                    about making minor adjustments to a song he mixed for me :)
+                                </p>
+                            </CardContent>
+                        </Card>
+
+                        <Card className="my-10">
+                            <CardHeader className="">
+                                <div className="flex justify-between">
+                                    <CardTitle className="text-1xl">Anna Doe</CardTitle>
+                                </div>
+                                <div className="flex justify-between text-sm text-muted-foreground">
+                                    <span>Client</span>
+                                    <span>12 months ago</span>
+                                </div>
+                            </CardHeader>
+                            <CardContent className="pt-2">
+                                <p className="text-justify">
+                                    My experience working with this guy was great! I really liked how passionate he was
+                                    about making minor adjustments to a song he mixed for me :)
+                                </p>
+                            </CardContent>
+                        </Card>
+                    </div>)
+            )}
+            {error &&
+                <Alert variant="destructive" className="mt-5">
                     <AlertCircle className="h-4 w-4"/>
                     <AlertTitle>Error</AlertTitle>
                     <AlertDescription>
                         {error}
                     </AlertDescription>
                 </Alert>
-            )}
+            }
         </div>
     );
 }
