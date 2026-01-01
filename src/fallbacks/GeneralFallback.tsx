0f721758e8e06708b3fd69b21a7986e5bf4b5e5b
Stanisław Stepka <s28016@pjwstk.edu.pl>
2026-01-01 22:54:23 +0100

    Fix for failing actions

--- src/fallbacks/GeneralFallback.tsx
+++ src/fallbacks/GeneralFallback.tsx
@@ -5,18 +5,18 @@ import {Button} from "@/components/ui/button.tsx";
 
 export const GeneralFallback = ({error, resetErrorBoundary}: FallbackProps) => {
     return (
-        <div className="p-50 gap-5 md: flex flex-col min-h-screen justify-center">
-            <div className="text-center">
-                <h1 className="text-5xl">We are sorry!</h1>
+            <div className="p-50 gap-5 md: flex flex-col min-h-screen justify-center">
+                <div className="text-center">
+                    <h1 className="text-5xl">We are sorry!</h1>
+                </div>
+                <Alert variant="destructive">
+                    <AlertCircle className="h-4 w-4"/>
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>
+                        Service is experiencing issues. Please read the following message {error.message}
+                    </AlertDescription>
+                </Alert>
+                <Button onClick={resetErrorBoundary}>Try to refresh the page</Button>
             </div>
-            <Alert variant="destructive">
-                <AlertCircle className="h-4 w-4"/>
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                    An error has occurred! Please read the following: {error}
-                </AlertDescription>
-            </Alert>
-            <Button onClick={resetErrorBoundary}>Try to refresh the page</Button>
-        </div>
     );
 }
